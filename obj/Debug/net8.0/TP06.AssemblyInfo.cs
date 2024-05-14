2d777d713837848c35cdc8e76df422c16ada184f
MiguelAngelBustos <chetobustos96@gmail.com>
2024-05-14 15:28:52 -0300

    Reinicio

--- obj/Debug/net8.0/TP06.AssemblyInfo.cs
+++ obj/Debug/net8.0/TP06.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TP06")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9d480e4b32461f55e89ae39979107bdc65742996")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c2cb31bfcd8c91a013d36c261ae395ebbc2cd42a")]
 [assembly: System.Reflection.AssemblyProductAttribute("TP06")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TP06")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
